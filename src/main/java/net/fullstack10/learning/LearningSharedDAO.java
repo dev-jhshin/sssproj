9cae7f56848e23b804740a0fd22d07ae2aa2ab37
hyeon48615 <hyeon48615@gmail.com>
2025-04-09 11:36:14 +0900

    fix: 공유된 사용자 출력 버그 수정

--- src/main/java/net/fullstack10/learning/LearningSharedDAO.java
+++ src/main/java/net/fullstack10/learning/LearningSharedDAO.java
@@ -100,7 +100,7 @@ public class LearningSharedDAO extends DBConnPool {
 				dto.setSharedTo(rs.getString("sharedTo"));
 				dto.setSharedToName(rs.getString("sharedToName"));
 				dto.setSharedFrom(rs.getString("sharedFrom"));
-				dto.setSharedFromName(rs.getString("sharedName"));
+				dto.setSharedFromName(rs.getString("sharedToName"));
 				dto.setCreatedAt(rs.getTimestamp("createdAt").toLocalDateTime());
 				
 				list.add(dto);
