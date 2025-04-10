811bec7a1bd1d3a42706054857476d5adbc8e13b
hyeon48615 <hyeon48615@gmail.com>
2025-04-10 11:34:48 +0900

    fix: 학습 게시글 오늘의 학습 기간 설정 버그 수정

--- src/main/java/net/fullstack10/learning/LearningRegistController.java
+++ src/main/java/net/fullstack10/learning/LearningRegistController.java
@@ -79,8 +79,10 @@ public class LearningRegistController extends HttpServlet {
 		if(!ValidationUtil.isLoggedIn(loginMemberId, response)) return;
 		if(!LearningValidationUtil.isValidTitle(learningTitle, response)) return;
 		if(!LearningValidationUtil.isValidContent(learningContent, response)) return;
-		if(!LearningValidationUtil.isValidVisibilityPeriod(isVisible, learningStartedAt, learningEndedAt, response)) return;
-		if(!LearningValidationUtil.isValidDateOrder(learningStartedAt, learningEndedAt, response)) return;
+		if ("Y".equals(isVisible)) {
+			if(!LearningValidationUtil.isValidVisibilityPeriod(isVisible, learningStartedAt, learningEndedAt, response)) return;
+			if(!LearningValidationUtil.isValidDateOrder(learningStartedAt, learningEndedAt, response)) return;
+		}
 
 		LearningDTO learningDTO = new LearningDTO();
 		learningDTO.setMemberId(loginMemberId);
