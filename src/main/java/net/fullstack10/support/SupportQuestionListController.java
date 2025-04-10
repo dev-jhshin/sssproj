60d0fb8dbc2d26789002c38e070b3c9410d6342f
godjihye <njih112@gmail.com>
2025-04-10 13:19:23 +0900

    Fix: paging - total page size가 1 미만이면 paging 안뜨게 수정

--- src/main/java/net/fullstack10/support/SupportQuestionListController.java
+++ src/main/java/net/fullstack10/support/SupportQuestionListController.java
@@ -12,6 +12,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import net.fullstack10.common.CommonUtil;
+import net.fullstack10.common.JSFunction;
 
 /**
  * Servlet implementation class SupportQuestionListController
@@ -34,46 +35,41 @@ public class SupportQuestionListController extends HttpServlet {
 	 */
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
 		request.setCharacterEncoding("UTF-8");
 		response.setContentType("text/html; charset=UTF-8");
-		PrintWriter wrt = response.getWriter();
 
 		String memberId = (String) request.getSession().getAttribute("memberId");
-		if(memberId ==null || memberId.length()<1) {
-			wrt.println("<script>");
-			wrt.println("alert('사용자 정보가 없습니다.');");
-			wrt.println("history.back();");
-			wrt.println("</script>");
-			wrt.close();
+		if(memberId == null || memberId.length() < 1) {
+			JSFunction.alertLocation(response, "로그인 후 이용해주세요.", "/sssproj/auth/login.do");
 			return;
 		}
 
 		supportDAO = new SupportDAO();
 		Map<String, Object> pMap = new HashMap<>();
+		
 		// 페이징 변수
 		String pageNo = cUtil.setPageParam(request.getParameter("page_no"), "1");
 		String pageSize = cUtil.setPageParam(request.getParameter("page_size"), "10");
 		String pageBlockSize = cUtil.setPageParam(request.getParameter("page_block_size"), "10");
 		int pageSkipCount = (cUtil.parseInt(pageNo) - 1) * cUtil.parseInt(pageSize);
+		
 		// 검색 변수
 		String searchCategory = cUtil.setSearchParam(request.getParameter("search_category"));
 		String searchWord = cUtil.setSearchParam(request.getParameter("search_word"));
-//		String searchStart = cUtil.setSearchParam(request.getParameter("search_start"));
-//		String searchEnd = cUtil.setSearchParam(request.getParameter("search_end"));
 		String queryString = "page_size=" + pageSize+"&page_block_size="+pageBlockSize;
 		queryString += "&search_category=" + (searchCategory != null && !searchCategory.isEmpty() ? searchCategory : "");
 		queryString += "&search_word=" + (searchWord != null && !searchWord.isEmpty() ? searchWord : "");
 
 		pMap.put("pageSkipCount", pageSkipCount);
 		pMap.put("pageSize", pageSize);
-
 		pMap.put("searchCategory", searchCategory);
 		pMap.put("searchWord", searchWord);
-//		pMap.put("searchStart", searchStart);
-//		pMap.put("searchEnd", searchEnd);
 		pMap.put("memberId", memberId);
+		
 		int totalCount = supportDAO.getInquirySize(pMap);
 		pMap.put("totalCount", totalCount);
+		
 		pMap.put("paging", SupportPage.pagingArea(totalCount, cUtil.parseInt(pageNo), cUtil.parseInt(pageSize), cUtil.parseInt(pageBlockSize), "list.do?" + queryString));
 		List<InquiryDTO> inquiries = supportDAO.getInquiryList(pMap);
 		supportDAO.close();
